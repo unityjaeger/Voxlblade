9c04d24845eb3d781e5f35b32984af6d0094f3d5
unityjaeger <69147315+unityjaeger@users.noreply.github.com>
2023-02-26 21:02:24 +0100

    Update esp.lua

--- libraries/esp.lua
+++ libraries/esp.lua
@@ -17,6 +17,7 @@ local Properties = {
     Visible = true,
     Color = Color3.new(1, 1, 1),
     Transparency = 1,
+    AutoRemove = true,
     Text = {
         Size = 18,
         Font = Drawing.Fonts.Monospace,
@@ -219,11 +220,13 @@ function ESP:Add(Type, Part, Properties)
 
     self.Holder[selfDrawing.Slot] = Structure
 
-    MainPart.AncestryChanged:Connect(function(_, new)
-        if not new then
-            selfDrawing:Remove()
-        end
-    end)
+    if self.InputProperties.AutoRemove then
+        MainPart.AncestryChanged:Connect(function(_, new)
+            if not new then
+                selfDrawing:Remove()
+            end
+        end)
+    end
 
     return selfDrawing
 end
