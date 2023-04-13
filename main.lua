a0f6ccfd00a4137ce4863dc118afa17adf774a3a
unityjaeger <69147315+unityjaeger@users.noreply.github.com>
2023-04-13 15:24:44 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,6 +4,9 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
+local RunService = game:GetService("RunService")
+local TweenService = game:GetService("TweenService")
+
 local Player = game.Players.LocalPlayer
 
 local Character = Player.Character or Player.CharacterAdded:Wait()
@@ -11,14 +14,33 @@ local Root = Character:WaitForChild("HumanoidRootPart")
 local Torso = Character:WaitForChild("Torso")
 local Humanoid = Character:WaitForChild("Humanoid")
 
+--for waiting for mobs to spawn: tween through the music zones until one spawns, if multiple zones then tween between them, if one zone then tween around the sides by size
+
+local Limbs = {}
+
+local function SetupNoclip()
+	for _,v in ipairs(getconnections(Torso.ChildAdded)) do
+		v:Disable()
+	end
+	for _,v in ipairs(Character:GetChildren()) do
+		if v:IsA("BasePart") then
+			table.insert(Limbs, v)
+		end
+	end
+end
+
 function Setup()
+    table.clear(Limbs)
     Character, Root, Torso, Humanoid = nil
     Character = Player.CharacterAdded:Wait()
     Root = Character:WaitForChild("HumanoidRootPart")
     Torso = Character:WaitForChild("Torso")
     Humanoid = Character:WaitForChild("Humanoid")
     Humanoid.Died:Connect(Setup)
+    SetupNoclip()
 end
+
+SetupNoclip()
 Humanoid.Died:Connect(Setup)
 
 local Lighting = game.Lighting
@@ -96,6 +118,41 @@ MainBox:AddButton({
     Tooltip = '...'
 })
 
+local CharacterHandler = require(Player.PlayerScripts.Core.Controllers.CharHandler)
+
+local DefaultSpeed, _ = CharacterHandler:GetSpeed()
+local DefaultRunSpeed = DefaultSpeed * 1.6
+
+MainBox:AddSlider('WalkSpeed', {
+    Text = 'Walkspeed',
+    Default = DefaultSpeed,
+    Min = DefaultSpeed,
+    Max = 60,
+    Rounding = 2,
+    Compact = false,
+})
+
+MainBox:AddSlider('RunSpeed', {
+    Text = 'Run Speed',
+    Default = DefaultRunSpeed,
+    Min = DefaultRunSpeed,
+    Max = 60,
+    Rounding = 2,
+    Compact = false,
+})
+
+Humanoid:GetPropertyChangedSignal("WalkSpeed"):Connect(function()
+    if CharacterHandler.Running then
+        if Options.RunSpeed.Value > DefaultRunSpeed then
+            Humanoid.WalkSpeed = Options.RunSpeed.Value
+        end
+    else
+        if Options.WalkSpeed.Value > DefaultSpeed then
+            Humanoid.WalkSpeed = Options.WalkSpeed.Value
+        end
+    end
+end)
+
 Toggles.NoFog:OnChanged(function(bool)
     if bool then
         Lighting.FogEnd = 9e9
@@ -128,3 +185,57 @@ MenuGroup:AddButton('Unload', function() Library:Unload() end)
 MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = true, Text = 'Menu keybind' }) 
 
 Library.ToggleKeybind = Options.MenuKeybind
+
+local NoclipConnection
+local function Noclip(bool)
+	if bool then
+		NoclipConnection = RunService.Stepped:Connect(function()
+			for _, Limb in ipairs(Limbs) do
+				Limb.CanCollide = false
+			end
+		end)
+	else
+		if NoclipConnection then
+			NoclipConnection:Diconnect()
+		end
+	end
+end
+
+local FloatingPart
+local FloatConnection
+local function Float(bool)
+    if bool then
+        FloatingPart = Instance.new("Part")
+        FloatingPart.Size = Vector3.new(5, 1, 5)
+        FloatingPart.Transparency = .5
+        FloatingPart.Anchored = true
+        FloatingPart.Parent = workspace.CurrentCamera
+        
+        local Offset = CFrame.new(0, -3.5, 0)
+        FloatConnection = RunService.Heartbeat:Connect(function()
+            if Root then
+                FloatingPart.CFrame = Root.CFrame * Offset
+            end
+        end)
+    else
+        if FloatingPart then
+            FloatingPart:Destroy()
+        end
+        if FloatConnection then
+            FloatConnection:Disconnect()
+        end
+    end
+end
+
+local function Transport(To)
+    if not Root then
+        return
+    end
+
+    local Distance = (Root.Position - To.Position).Magnitude
+    if Distance > 25 then
+        local Tween = TweenService:Create(Root, TweenInfo.new(Distance / 100, Enum.EasingStyle.Linear), {CFrame = To})
+    else
+        Root.CFrame = To
+    end
+end
